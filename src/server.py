21db85d0f77d4b3e6c76399373a28a16776edd2b
ahio <stepanlopushanskyi@Stepans-MacBook-Pro.local>
2020-03-02 17:32:19 +0200

    small code refactoring

--- src/server.py
+++ src/server.py
@@ -156,14 +156,5 @@ try:
 finally:
     print(cert_path)
 
-print(f'\033[92m {"-"*150}\n \033[0m')
-print(f'\033[92m   WELCOME TO VGS DEMO APPLICATION \U0001F440 \n\n')
-print(f'\033[92m   To run application follow next steps:\n')
-print(f'\033[92m   1. Copy PUBLIC URL \033[0m {https_public_url}\n')
-print(f'\033[92m   2. OPEN inbound route \033[0m {os.getenv("INBOUND_ROUTE_LINK")}\n')
-print(f'\033[92m   3. Set up PUBLIC URL as upstream host\n')
-print(f'\033[92m   4. Open PUBLIC URL \033[0m {https_public_url}\n')
-print(f'\033[92m {"-"*150}\n \033[0m')
-
 if __name__ == '__main__':
     app.run(port=3000, host='0.0.0.0', debug=False)
